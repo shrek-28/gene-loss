ebf62b554e71236da61af7d317b7bd3d9539df4a
Shreyasree G <shreyagopal28@gmail.com>
2026-02-26 16:11:50 +0530

    data download scripts

---
+++ scripts/download_data.sh
@@ -0,0 +1,20 @@
+# ## downloading genome FASTA
+# wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.40_GRCh38.p14/GCF_000001405.40_GRCh38.p14_genomic.fna.gz
+
+# ## downlaoding protein FAST
+# wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.40_GRCh38.p14/GCF_000001405.40_GRCh38.p14_protein.faa.gz
+
+# ## genome GTF file 
+# wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.40_GRCh38.p14/GCF_000001405.40_GRCh38.p14_genomic.gtf.gz
+
+# ## genome GFF file 
+# wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.40_GRCh38.p14/GCF_000001405.40_GRCh38.p14_genomic.gff.gz
+
+## script used to run in loop:
+
+for f in genomic.fna.gz protein.faa.gz genomic.gtf.gz genomic.gff.gz
+do
+    wget -c https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.40_GRCh38.p14/GCF_000001405.40_GRCh38.p14_$f
+done
+
+gunzip *.gz
